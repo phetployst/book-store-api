03153b5239e7220267a8d6f1fb8fe9ca961a4260
phetploy <st.phetploy@gmail.com>
2024-09-02 23:35:49 +0700

    :sparkles: add POST /books route for creating book

--- router/router.go
+++ router/router.go
@@ -4,10 +4,16 @@ import (
 	"net/http"
 
 	"github.com/labstack/echo/v4"
+	"github.com/phetployst/book-store-api/book"
+	"gorm.io/gorm"
 )
 
-func RegisterRoutes(e *echo.Echo) {
+func RegisterRoutes(e *echo.Echo, db *gorm.DB) {
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
+
+	bookHandler := book.NewHandler(db)
+
+	e.POST("/books", bookHandler.Create)
 }
--- router/router_test.go
+++ router/router_test.go
@@ -3,20 +3,42 @@ package router
 import (
 	"net/http"
 	"net/http/httptest"
+	"reflect"
 	"testing"
 
 	"github.com/labstack/echo/v4"
 )
 
+type Route struct {
+	Path   string
+	Method string
+}
+
 func TestRegisterRoutes(t *testing.T) {
 	e := echo.New()
+	defer e.Close()
+
+	RegisterRoutes(e, nil)
+
 	request := httptest.NewRequest(http.MethodGet, "/", nil)
 	response := httptest.NewRecorder()
 	e.ServeHTTP(response, request)
-	RegisterRoutes(e)
+
 	routes := e.Routes()
-	want := 1
-	if len(routes) != 1 {
-		t.Errorf("got %d routes but want %d routes", len(routes), want)
+
+	got := make([]Route, len(routes))
+	for i, route := range routes {
+		got[i] = Route{
+			Path:   route.Path,
+			Method: route.Method,
+		}
+	}
+
+	want := []Route{
+		{"/", http.MethodGet},
+		{"/books", http.MethodPost},
+	}
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v routes but want %v routes", got, want)
 	}
 }
